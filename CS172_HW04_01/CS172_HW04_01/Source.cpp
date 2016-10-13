72ed4aee3fef24d585e1bf6f2a02e71e413b37a1
Ian Fisher <ifisher18@my.whitworth.edu>
2016-10-12 20:29:10 -0700

    Outlined general structure

--- CS172_HW04_01/CS172_HW04_01/Source.cpp
+++ CS172_HW04_01/CS172_HW04_01/Source.cpp
@@ -4,3 +4,30 @@
 // 10/12/16
 ///////////////////////////////
 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Welcome! This is a math program that takes the average of a given set of numbers." << endl;
+	cout << endl;
+	cout << "Enter the amount of numbers that you'd like to enter: ";
+	cin >> num_size;
+	cout << endl;
+	cout << "Now enter each number that you'd like to find the average of: ";
+	// for loop to enter values into array
+
+
+}
+
+// function that calculates the average of the user-entered numbers
+double numAver ()
+{
+	return 
+}
+
+// function that find out how many numbers are above the average
+int numAbove ()
+{
+	return 
+}

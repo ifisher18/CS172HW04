172902aea71e0ea5d43d4f3873aff492a67c378b
Ian Fisher <ifisher18@my.whitworth.edu>
2016-10-12 20:38:48 -0700

    Initial Commit

---
+++ CS172_HW04_EX04_03/CS172_HW04_EX04_03/Source.cpp
@@ -0,0 +1,15 @@
+///////////////////////////////
+// Ian Fisher
+// CS 172 HW04 EX03
+// 10/12/16
+///////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+// find the smallest element
+
+int main ()
+{
+	
+}

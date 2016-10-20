28d24e817e3a3e9f83d2f86f74d0fb9eb2b65530
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-10-20 15:52:29 -0700

    final comments-- DONE

--- CS172_HW04_01/CS172_HW04_01/Source.cpp
+++ CS172_HW04_01/CS172_HW04_01/Source.cpp
@@ -12,24 +12,24 @@ int numAbove(int*nums, int sizenum);
 int main ()
 {
 	int num_size; // creates a variable called num_size that 
-	cout << "Welcome! This is a math program that takes the average of a given set of numbers." << endl;
+	cout << "**Welcome! This is a math program that takes the average of a given set of numbers.**" << endl; // prompts to user the purpose of this program
 	cout << endl;
 	cout << "Enter the amount of numbers that you'd like to enter: ";
 	cin >> num_size;
 	cout << endl;
 	cout << "Now enter each number that you'd like to find the average of: ";
-	int numbers[100];
+	int numbers[100]; // creates an array of the entered numbers
 
 	// loop that enters each number into array
 	for (int i = 0; i < num_size; i++) { 
-		cin >> numbers[i];
+		cin >> numbers[i]; // cin's each entry to array
 	}
 
 	// outputs the average of the numbers in the given array
-	cout << "The average of the entered numbers is: " << numAver(numbers, num_size) << "." << endl;
+	cout << "The average of the entered numbers is: " << numAver(numbers, num_size) << "." << endl; // calls the average function given the parameters of numbers and num_size
 
 	// outputs the amount of numbers in the array that are above the average
-	cout << "The amount of numbers that is above the calculated average is: " << numAbove(numbers, num_size) << "." << endl;
+	cout << "The amount of numbers that is above the calculated average is: " << numAbove(numbers, num_size) << "." << endl; // calls the numAbove function which compares each number to the average
 
 	return 0;
 
@@ -38,7 +38,7 @@ int main ()
 // function that calculates the average of the user-entered numbers in array "numbers"
 double numAver(int*nums, int sizenum)
 {
-	int sum = 0;
+	int sum = 0; // sets the initial sum to 0-- but will change based on values inputed.
 	for (int i = 0; i < sizenum; i++) { // loop that checks through the array
 		sum += *(nums + i); // x = x + *(nums + i) (adds each number in array to each other)
 	}
@@ -49,12 +49,12 @@ double numAver(int*nums, int sizenum)
 // function that find out how many numbers are above the average in the array "numbers"
 int numAbove (int*nums, int sizenum)
 {
-	int checknum = 0;
-	double x = numAver(nums, sizenum);
+	int checknum = 0; // declares an incrementor to keep track of how many numbers are above average
+	double x = numAver(nums, sizenum); // creates a variable x that equals the average
 	for (int i = 0; i < sizenum; i++) { // loop that checks through the array
 		if ((double)*(nums + i) > x) { // is the same as (double)nums[i]
-			checknum++;
+			checknum++; // inc. the checknum if the number is above average
 		}
 	}
-	return checknum;
+	return checknum; // returns the total number of integers above the average
 }

803980296859f65e9929f7528434345e9789e2b0
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-10-20 15:42:06 -0700

    Almost done-- made signifigant changes

--- CS172_HW04_01/CS172_HW04_01/Source.cpp
+++ CS172_HW04_01/CS172_HW04_01/Source.cpp
@@ -7,27 +7,54 @@
 #include <iostream>
 using namespace std;
 
+double numAver(int*nums, int sizenum);
+int numAbove(int*nums, int sizenum);
 int main ()
 {
+	int num_size; // creates a variable called num_size that 
 	cout << "Welcome! This is a math program that takes the average of a given set of numbers." << endl;
 	cout << endl;
 	cout << "Enter the amount of numbers that you'd like to enter: ";
 	cin >> num_size;
 	cout << endl;
 	cout << "Now enter each number that you'd like to find the average of: ";
-	// for loop to enter values into array
+	int numbers[100];
 
+	// loop that enters each number into array
+	for (int i = 0; i < num_size; i++) { 
+		cin >> numbers[i];
+	}
+
+	// outputs the average of the numbers in the given array
+	cout << "The average of the entered numbers is: " << numAver(numbers, num_size) << "." << endl;
+
+	// outputs the amount of numbers in the array that are above the average
+	cout << "The amount of numbers that is above the calculated average is: " << numAbove(numbers, num_size) << "." << endl;
+
+	return 0;
 
 }
 
-// function that calculates the average of the user-entered numbers
-double numAver ()
+// function that calculates the average of the user-entered numbers in array "numbers"
+double numAver(int*nums, int sizenum)
 {
-	return 
+	int sum = 0;
+	for (int i = 0; i < sizenum; i++) { // loop that checks through the array
+		sum += *(nums + i); // x = x + *(nums + i) (adds each number in array to each other)
+	}
+	 
+	return ((double) sum / sizenum); // casts x to a double value so decimals will be included-- this is the basic average calculation: sum/number of nums
 }
 
-// function that find out how many numbers are above the average
-int numAbove ()
+// function that find out how many numbers are above the average in the array "numbers"
+int numAbove (int*nums, int sizenum)
 {
-	return 
+	int checknum = 0;
+	double x = numAver(nums, sizenum);
+	for (int i = 0; i < sizenum; i++) { // loop that checks through the array
+		if ((double)*(nums + i) > x) { // is the same as (double)nums[i]
+			checknum++;
+		}
+	}
+	return checknum;
 }

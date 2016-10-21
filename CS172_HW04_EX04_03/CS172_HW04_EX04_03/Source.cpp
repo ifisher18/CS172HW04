61563370a24eef091a1298304cd66884b669fa13
ifisher18 <ifisher18@LAB-SCI-304-13>
2016-10-20 20:02:04 -0700

    Some changes

--- CS172_HW04_EX04_03/CS172_HW04_EX04_03/Source.cpp
+++ CS172_HW04_EX04_03/CS172_HW04_EX04_03/Source.cpp
@@ -8,8 +8,29 @@
 using namespace std;
 
 // find the smallest element
+int smallestNum(int*nums);
 
 int main ()
 {
-	
+	cout << "**Welcome! This program finds the smallest element in an array of integers.**" << endl;
+	cout << endl;
+	cout << "Enter a series of integers: ";
+	int numbers[100];
+
+	// loops that enters each number into the array
+	for (int i = 0; i < 100; i++)
+	{
+		cin >> numbers[i]; // cin's each entry into the array
+	}
+
+	cout << "The smallest element in this list of numbers is: " << smallestNum(numbers) << "." << endl;
+
+}
+
+int smallestNum (int*nums)
+{
+	for (int i = 0; i < 100; i++)
+	{
+		if ()
+	}
 }

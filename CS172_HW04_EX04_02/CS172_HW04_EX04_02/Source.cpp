ae176a76a99856387ee6e582cec2529d121f04e2
ifisher18 <ifisher18@LAB-SCI-304-13>
2016-10-20 19:47:24 -0700

    Major changes, comments

--- CS172_HW04_EX04_02/CS172_HW04_EX04_02/Source.cpp
+++ CS172_HW04_EX04_02/CS172_HW04_EX04_02/Source.cpp
@@ -7,4 +7,24 @@
 #include <iostream>
 using namespace std;
 
-/// Look at notes on HW sheet
+// Function that doubles the size of an array
+int* doubleCapacity(const int* list, int size)
+{
+	int* Doublearray = new int[(2 * size)]; // creates a pointer that doubles the size of each memory location
+	for (int i = 0; i < size; i++) // for loop that accesses the array and creates a copy of Doublearray in list
+	{
+		Doublearray[i] = list[i]; // copy of Doublearray in list
+	}
+	delete list;
+	return Doublearray; // returns the new array of twice the size
+}
+
+int main ()
+{
+	int*list = new int[3];
+	// {1, 2, 3}
+
+	list = doubleCapacity(list, 3);
+	delete list;
+	return 0;
+}

713f90a8527abcdb906f2a19bf3db1caff84c25b
ifisher18 <ifisher18@my.whitworth.edu>
2016-10-20 21:31:24 -0700

    Major changes-- almost done, comments

--- CS172_HW04_EX04_03/CS172_HW04_EX04_03/Source.cpp
+++ CS172_HW04_EX04_03/CS172_HW04_EX04_03/Source.cpp
@@ -5,6 +5,7 @@
 ///////////////////////////////
 
 #include <iostream>
+#include <limits>
 using namespace std;
 
 // find the smallest element
@@ -12,26 +13,32 @@ int smallestNum(int*nums);
 
 int main ()
 {
-	cout << "**Welcome! This program finds the smallest element in an array of integers.**" << endl;
-	cout << endl;
-	cout << "Enter a series of integers: ";
-	int numbers[100];
-
-	// loops that enters each number into the array
-	for (int i = 0; i < 100; i++)
-	{
-		cin >> numbers[i]; // cin's each entry into the array
-	}
-
-	cout << "The smallest element in this list of numbers is: " << smallestNum(numbers) << "." << endl; // cout statement that outputs the smallest number of the inputed values
-
+    // prompts the user to enter values into an array
+    cout << "** Welcome! This program finds the smallest element in an array of integers. **" << endl;
+    cout << endl;
+    cout << "Enter a series of integers: ";
+    int numbers[8]; // creates an array of size 8
+    
+    // loops that enters each number into the array
+    for (int i = 0; i < 8; i++)
+    {
+        cin >> numbers[i]; // cin's each entry into the array
+    }
+    
+    cout << "The smallest element in this list of numbers is: " << smallestNum(numbers) << "." << endl; // cout statement that outputs the smallest number of the inputed values
+    
 }
 
 // function that returns the smallest number after comparing to the entire array
-int smallestNum (int*nums)
+int smallestNum (int*nums) // function with the parameter of the pointer to each number
 {
-	for (int i = 0; i < 100; i++)
-	{
-		if ()
-	}
+    int currentsmall = numeric_limits<int>::max(); // sets the intial value equal to greatest value in C++
+    for (int i = 0; i < 8; i++) // for loop that checks each value in the array
+    {
+        if (nums[i] < currentsmall) { // if statement that checks if the current value in array is less than the current smallest number
+            currentsmall = nums[i]; // if it is less, then the smallest number is set to that value
+        }
+        
+    }
+    return currentsmall; // returns the smallest value after the loop is complete.
 }

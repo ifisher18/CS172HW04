6919021ccffe6675ad6733f0c1a07ccc5c1f04e4
ifisher18 <ifisher18@my.whitworth.edu>
2016-10-24 22:51:25 -0700

    Updates to function definitions

--- CS172_HW04_EX04_05/CS172_HW04_EX04_05/Course.h
+++ CS172_HW04_EX04_05/CS172_HW04_EX04_05/Course.h
@@ -1,3 +1,14 @@
+//
+//  Header.h
+//  hahahaha
+//
+//  Created by Ian J. Fisher on 10/24/16.
+//  Copyright © 2016 Ian J. Fisher. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
 #pragma once
 
 #include <string>
@@ -6,17 +17,21 @@ using namespace std;
 class Course
 {
 public:
-	Course(const string& courseName, int capacity);
-	~Course();
-	string getCourseName() const;
-	void addStudent(const string& name);
-	void dropStudent(const string& name);
-	string* getStudents() const;
-	int getNumberOfStudents() const;
-
+    Course(const string& courseName, int capacity);
+    Course(const Course &copy);
+    ~Course();
+    string getCourseName() const;
+    void addStudent(const string& name);
+    void dropStudent(const string& name);
+    string* getStudents() const;
+    int getNumberOfStudents() const;
+    void clear();
+    
 private:
-	string courseName;
-	string* students;
-	int numberOfStudents;
-	int capacity;
+    string courseName;
+    string* students;
+    int numberOfStudents;
+    int capacity;
 };
+
+#endif /* Header_h */

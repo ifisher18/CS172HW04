cecc57063e9127930034e47347d5c5b3f1e28e13
Ian Fisher <ifisher18@my.whitworth.edu>
2016-10-24 20:23:45 -0700

    Finished the first bullet point

--- CS172_HW04_EX04_05/CS172_HW04_EX04_05/Source.cpp
+++ CS172_HW04_EX04_05/CS172_HW04_EX04_05/Source.cpp
@@ -30,21 +30,22 @@ string Course::getCourseName() const
 
 void Course::addStudent(const string& name)
 {
-	students[numberOfStudents] = name;
-	numberOfStudents++;
-
 	// increase the array size if the number of students in the array exceeds array's capacity...? hmmm
-	// Function that doubles the size of an array
-	int* doubleCapacity(const int* list, int size)
+	// Function that doubles the size of an array, in this case students
+	if (numberOfStudents > students->size())
 	{
-		int* Doublearray = new int[(2 * size)]; // creates a pointer that doubles the size of each memory location
-		for (int i = 0; i < size; i++) // for loop that accesses the array and creates a copy of Doublearray in list
+		string* Doublearray = new string[(2 * numberOfStudents)]; // creates a pointer that doubles the size of each memory location
+		for (int i = 0; i < numberOfStudents; i++) // for loop that accesses the array and creates a copy of Doublearray in list
 		{
-			Doublearray[i] = list[i]; // copy of Doublearray in list
+			Doublearray[i] = students[i]; // copy of Doublearray in students
 		}
-		delete list; // deletes list since it is old memory space
-		return Doublearray; // returns the new array of twice the size..
+		students = Doublearray; // asigns students to Doublearray (twice the size)
+		
+		delete[] Doublearray; // deletes students since it is old memory space
+		 // returns the new array of twice the size..
 	}
+		students[numberOfStudents] = name;
+		numberOfStudents++;
 }
 
 void Course::dropStudent(const string& name)
@@ -66,6 +67,7 @@ void clear()
 {
 	// removes all students from the course
 }
+
 
 int main ()
 {
